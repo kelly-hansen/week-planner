b34e0d8574c811fe422d701f8d81fa7a81495429
Rachel Beacham <rachelbeacham17@gmail.com>
2020-10-30 14:56:29 -0700

    event listeners

--- main.js
+++ main.js
@@ -11,13 +11,29 @@ var data = {
   }
 };
 
+var $addEntryBtn = document.querySelector('#addEntryBtn');
+var $modalContainer = document.querySelector('.modal-container');
+var $entrySubmitBtn = document.querySelector('#entry-submit-btn');
+var $addEntryForm = document.querySelector('.add-entry-form');
+
+$addEntryBtn.addEventListener('click', function(e){
+  $modalContainer.className = 'modal-container';
+});
+
+$entrySubmitBtn.addEventListener('click', function(){
+  $modalContainer.className = 'modal-container hidden';
+  $addEntryForm.reset();
+});
+
+
 window.addEventListener('beforeunload', function () {
   var dataJson = JSON.stringify(data);
   localStorage.setItem('data-object', dataJson);
 });
 
-var previousDataJson = localStorage.getItem('data-object');
-
-if (previousDataJson !== null) {
-  data = JSON.parse(previousDataJson);
-}
+window.addEventListener('DOMContentLoaded', function(){
+  var previousDataJson = localStorage.getItem('data-object');
+  if (previousDataJson !== null) {
+    data = JSON.parse(previousDataJson);
+  }
+});
